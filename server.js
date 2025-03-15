36b0705d16f3dabeb80edcfda533dc11935d7d1a
Priyanshu kumar <priyanshu61iitj@gmail.com>
2025-03-15 22:31:15 +0530

    Setup

---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const path = require("path");
+const fastify = require("fastify")({ logger: true });
+const fastifyEnv = require("@fastify/env");
+
+
+//register plugins
+fastify.register(require("@fastify/cors"));
+fastify.register(require("@fastify/sensible"));
+
+
+fastify.register(require("@fastify/env"), {
+  dotenv: true,
+  schema: {
+    type: "object",
+    required: ["PORT", "MONGODB_URI", "JWT_TOKEN"],
+    properties: {
+      PORT: { type: "string", default: 3000 },
+      MONGODB_URI: { type: "string" },
+      JWT_TOKEN: { type: "string" },
+    },
+  },
+});
+
+//register custom plugins
+
+
+// Declare a route
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+//test database connection
+
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT });
+    fastify.log.info(
+      `Server is running at http://localhost:${process.env.PORT}`
+    );
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
